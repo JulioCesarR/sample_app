759da570544529794cd594315c937a00bdbb4400
Julio Ribeiro <ribeiro.julio12@gmail.com>
2017-12-07 16:00:06 -0200

    Add Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,18 @@
-class StaticPagesController < ApplicationController
+class StaticPagesController < ApplicationController 
 
-  def home
-    if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
-  end
+  def home 
+    if logged_in? 
+      @micropost  = current_user.microposts.build 
+      @feed_items = current_user.feed.paginate(page: params[:page]) 
+    end 
+  end 
 
-  def help
-  end
+  def help 
+  end 
 
-  def about
-  end
+  def about 
+  end 
 
-  def contact
-  end
-end
+  def contact 
+  end 
+end 
